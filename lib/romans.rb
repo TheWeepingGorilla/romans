229b414b541f75b1fef578e150ec0aa1cf5bd5db
Griffin Wimminger & Sean Morrison <student@epicodus.com>
2014-07-22 10:15:23 -0700

    converts 1000, 500, and 100

--- lib/romans.rb
+++ lib/romans.rb
@@ -2,5 +2,9 @@ def romans(number)
   numeral = []
   if number >= 1000
     "M"
+  elsif number >= 500
+    "D"
+  elsif number >= 100
+    "C"
   end
 end
--- spec/romans_spec.rb
+++ spec/romans_spec.rb
@@ -8,4 +8,7 @@ describe("romans") do
   it("converts the arabic numeral 500 to D") do
     romans(500).should(eq("D"))
   end
+  it("converts the arabic numeral 100 to C") do
+    romans(100).should(eq("C"))
+  end
 end
