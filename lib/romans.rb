d91cf0771945f3cb13df8e3d49848ddfaf9864e2
Griffin Wimminger & Sean Morrison <student@epicodus.com>
2014-07-22 10:19:59 -0700

    Converts 1000, 500, 100, 50, 10, 5, and 1 to roman equivalents

--- lib/romans.rb
+++ lib/romans.rb
@@ -6,5 +6,13 @@ def romans(number)
     "D"
   elsif number >= 100
     "C"
+  elsif number >= 50
+    "L"
+  elsif number >= 10
+    "X"
+  elsif number >= 5
+    "V"
+  elsif number >= 1
+    "I"
   end
 end
--- spec/romans_spec.rb
+++ spec/romans_spec.rb
@@ -11,4 +11,16 @@ describe("romans") do
   it("converts the arabic numeral 100 to C") do
     romans(100).should(eq("C"))
   end
+  it("converts the arabic numeral 50 to L") do
+    romans(50).should(eq("L"))
+  end
+  it("converts the arabic numeral 10 to X") do
+    romans(10).should(eq("X"))
+  end
+  it("converts the arabic numeral 5 to V") do
+    romans(5).should(eq("V"))
+  end
+  it("converts the arabic numeral 1 to I") do
+    romans(1).should(eq("I"))
+  end
 end
